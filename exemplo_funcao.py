64cdf3b9a875d2fd6cdc5465d6dcbc8d7099ae44
wallisonfer <wallisonsp.santos@gmail.com>
2022-05-05 22:54:15 -0300

    exemplo funcao mmc

--- exemplo_funcao.py
+++ exemplo_funcao.py
@@ -1,5 +1,14 @@
 def divisao(a, b):
     return a // b, a % b
 
+def mmc(a, b):
+    multiplo = a
+    while multiplo % a != 0 or multiplo % b != 0:
+        multiplo = multiplo + 1
+    
+    return multiplo
+
+print(mmc(28, 16))
+    
 res = divisao(5,3)
-print (res)
+print(res)
