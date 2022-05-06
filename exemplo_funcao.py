9c62290ee58d7a999471ac0f2c9a34831836558f
EduardaVigo <rm93596@fiap.com.br>
2022-05-05 22:55:43 -0300

    função

--- exemplo_funcao.py
+++ exemplo_funcao.py
@@ -1,8 +1,18 @@
+from typing import MutableMapping
 from pyrsistent import b
 
 
 def divisao(a, b):
     return a // b, a % b
+
+def mmc(a, b):
+    mutiplo = a
+    while mutiplo % a != 0 and mutiplo % b !=0:
+        mutiplo = mutiplo + 1
+
+    return mutiplo
+
+print(mmc(28, 16))
 
 res = divisao(5,3)
 print(res)
